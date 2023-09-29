1942b0de69a00287458b3ce69943b9b445513e4e
prettyleka <prettyleka@gmail.com>
2023-09-29 17:44:13 +0300

    Basic Authentication

--- app.js
+++ app.js
@@ -24,8 +24,33 @@ connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
+//authentification
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
 
 var app = express();
+app.use(auth);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -59,5 +84,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
